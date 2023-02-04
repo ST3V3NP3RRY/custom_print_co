e909c41d4fcd842c8e0defcb279ede314732c73a
Steven perry <Sperry3006@gmail.com>
2023-02-04 20:35:56 +0000

    updated banner styling

--- src/components/Reusable/Banner.jsx
+++ src/components/Reusable/Banner.jsx
@@ -1,6 +1,6 @@
 const Banner = ({ text }) => {
   return (
-    <div className="h-48 w-screen bg-red-200 flex items-center justify-center">
+    <div className="h-48 w-screen bg-gradient-to-r from-red-400 to-red-300 flex items-center justify-center">
       <h2 className="text-5xl">{text}</h2>
     </div>
   );
