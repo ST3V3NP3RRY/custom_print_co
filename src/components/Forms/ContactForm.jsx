3cc4a119a044d147b70d192726a2bdebb3a2b164
Steven perry <Sperry3006@gmail.com>
2023-02-04 17:17:08 +0000

    added contact button to form

--- src/components/Forms/ContactForm.jsx
+++ src/components/Forms/ContactForm.jsx
@@ -15,6 +15,12 @@ const ContactForm = () => {
 
       <label for="message">Send us a message</label>
       <textarea id="message" name="message" placeholder="Your message..." />
+      <button
+        type="submit"
+        class="md:w-32 bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-indigo-500 transition ease-in-out duration-300"
+      >
+        Submit
+      </button>
     </form>
   );
 };
