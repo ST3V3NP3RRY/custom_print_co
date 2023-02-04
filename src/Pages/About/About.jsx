077a47657fc112c3a8ae9c5b149a2c0e7bf52c12
Steven perry <Sperry3006@gmail.com>
2023-02-04 18:10:33 +0000

    added image box to about us page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -4,6 +4,9 @@ const About = () => {
   return (
     <>
       <Banner text={"About Us"} />
+      <section className="flex justify-center mt-12">
+        <div className="bg-slate-300 rounded-lg w-96 h-64"></div>
+      </section>
     </>
   );
 };
