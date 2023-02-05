1f6d0a093d484aa579a4d0e9dd3619c228cf4d1e
Steven perry <Sperry3006@gmail.com>
2023-02-05 12:15:35 +0000

    Added description to about page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -6,6 +6,10 @@ const About = () => {
       <Banner text={"About Us"} />
       <section className="flex justify-center mt-12">
         <div className="bg-slate-200 rounded-lg w-96 h-64 rounded-lg"></div>
+        <p className="w-96 text-justify ml-8">
+          We're a family run business specialising in custom made printed
+          products. We love flexing our creative muscles to provide .
+        </p>
       </section>
     </>
   );
